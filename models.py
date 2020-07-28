e48a5813fb5e2203debf563248e2a5954cf67919
Sebastian Contreras <sacm1046@gmail.com>
2020-07-27 22:49:13 -0400

    first commit

---
+++ models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(255), nullable=True)
+    email = db.Column(db.String(50), nullable=True)
+    password = db.Column(db.String(50), nullable=True)
+
+    def __repr__(self):
+        return 'User %r' % self.fullname
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'fullname': self.fullname,
+            'email': self.email
+        }
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship(User, backref=backref('children', cascade='all, delete'))
+
+    def __repr__(self):
+        return 'Score %r' % self.content
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'content': self.content,
+            'user': self.user.serialize()
+        }
---
+++ routes/posts.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, jsonify, request
+from models import db, Post
+
+post_route = Blueprint('post_route', __name__)
+@post_route.route('/posts', methods=['GET'])
+@post_route.route('/post/<int:id>', methods=['GET'])
+def getpost(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            post = Post.query.get(id)
+            if post:
+                return jsonify(post.serialize()), 200
+            else:
+                return jsonify({"error":"Post no encontrado"}), 404
+        else:
+            posts = Post.query.all()
+            posts = list(map(lambda post:post.serialize(), posts))
+            return jsonify(posts), 200
+
+@post_route.route('/posts', methods=['POST'])
+def postpost():   
+    content = request.json.get('content')   
+    user_id = request.json.get('user_id')
+    if not content:
+        return jsonify({"error": "Inserte su contenido"}), 422
+    if not user_id:
+        return jsonify({"error": "Inserte su id de usuario"}), 422 
+    post = Post()
+    post.content = content
+    post.user_id = user_id
+    db.session.add(post)
+    db.session.commit()
+    return jsonify(post.serialize()), 201
+
+@post_route.route('/post/<int:id>', methods=['PUT'])
+def updatepost(id):
+    if request.method == 'PUT':
+        post = Post.query.get(id)            
+        post.content = request.json.get('name')
+        db.session.commit()
+        return jsonify(post.serialize()), 201
+
+@post_route.route('/post/<int:id>', methods=['DELETE'])
+def deletepost(id):
+    if request.method == 'DELETE':
+        post = Post.query.get(id)
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({"success":"Post borrado exitósamente"}), 200
