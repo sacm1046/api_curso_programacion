328a855ada8f543daf06428f6f59960306dc665c
sacm <sacm1046@gmail.com>
2020-08-02 04:26:56 +0000

    from PA

--- routes/posts.py
+++ routes/posts.py
@@ -18,31 +18,37 @@ def getpost(id=None):
             return jsonify(posts), 200
 
 @post_route.route('/posts', methods=['POST'])
-def postpost():   
-    content = request.json.get('content')   
-    user = request.json.get('user')  
+def postpost():
+    content = request.json.get('content')
+    user = request.json.get('user')
     #user_id = request.json.get('user_id')
-    if not content:
-        return jsonify({"error": "Inserte su contenido"}), 422
     if not user:
-        return jsonify({"error": "Inserte su usuario"}), 422
+        return jsonify({"error": "Usuario es obligatorio"}), 422
+    if not content:
+        return jsonify({"error": "Contenido es obligatorio"}), 422
     #if not user_id:
-    #    return jsonify({"error": "Inserte su id de usuario"}), 422 
+    #    return jsonify({"error": "Inserte su id de usuario"}), 422
     post = Post()
     post.content = content
     post.user = user
     #post.user_id = user_id
     db.session.add(post)
     db.session.commit()
-    return jsonify(post.serialize()), 201
+    return jsonify({
+        "post": post.serialize(),
+        "success": "Post creado exitósamente"
+        }), 201
 
 @post_route.route('/post/<int:id>', methods=['PUT'])
 def updatepost(id):
     if request.method == 'PUT':
-        post = Post.query.get(id)            
+        post = Post.query.get(id)
         post.content = request.json.get('content')
         db.session.commit()
-        return jsonify(post.serialize()), 201
+        return jsonify({
+            "post": post.serialize(),
+            "success": "Post editado exitósamente"
+        }), 201
 
 @post_route.route('/post/<int:id>', methods=['DELETE'])
 def deletepost(id):
